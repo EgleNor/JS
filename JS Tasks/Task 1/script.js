c7587f1c7f1d95dee87e99d4e81f47ebe5cacc4a
EgleNor <egle.norkute.mail@gmail.com>
2022-12-07 20:51:41 +0200

    atlikta

--- JS Tasks/Task 1/script.js
+++ JS Tasks/Task 1/script.js
@@ -8,3 +8,21 @@ pamatyti jo pateikto svorio konvertavimą į:
 Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 <div id="output"></div> viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
+const getResult = document.querySelector('#submit-btn');
+const result = document.querySelector('#output');
+
+getResult.addEventListener('click', (e) => {
+  e.preventDefault();
+  const kilograms = document.querySelector('#search').value;
+  result.innerHTML = '';
+  const lb = (kilograms * 2.2046).toFixed(2);
+  const g = (kilograms / 0.0010000).toFixed(2);
+  const oz = (kilograms * 35.274).toFixed(2);
+
+  result.innerHTML =
+    `
+    <h2>${kilograms} kg = ${lb} lb</h2>
+    <h2>${kilograms} kg = ${g} g</h2>
+    <h2>${kilograms} kg = ${oz} oz</h2>
+    `
+});
